e263de43362ad925338369a54b0b093a5efcee98
Wiktor Plaga <wiktor.plaga@tzif.io>
2025-10-08 13:56:12 +0200

    Add content_items_sources db schema

--- apps/content-proxy/db/schemas/content.ts
+++ apps/content-proxy/db/schemas/content.ts
@@ -1,4 +1,4 @@
-import { timestamp, varchar } from 'drizzle-orm/pg-core';
+import { boolean, jsonb, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { contentProxySchema } from '../schema';
 
 export const publishers = contentProxySchema.table('publishers', {
@@ -22,6 +22,76 @@ export const publishers = contentProxySchema.table('publishers', {
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
 
+export const contentItemsSources = contentProxySchema.table(
+  'content_items_sources',
+  {
+    /**
+     * FIXME: I don't think we ACTUALLY use the UUID v4 format here.
+     * Should we?
+     * Unique identifier for the content item source (UUID v4)
+     * @example "72f20c838bb0"
+     * @example "9d12fa6e1c84"
+     */
+    id: varchar('id', { length: 12 }).primaryKey().notNull(),
+
+    /**
+     * Foreign key reference to the publisher
+     */
+    publisherId: varchar('publisher_id', { length: 12 })
+      .notNull()
+      .references(() => publishers.id),
+
+    /**
+     * Type of content item source
+     * @example "csv"
+     * @example "ftp"
+     * @example "s3"
+     * @example "rss-podcast"
+     * @example "google-sheet"
+     * @example "youtube-channel"
+     * @example "youtube-playlist"
+     */
+    type: varchar('type', { length: 256 }).notNull(),
+
+    /**
+     * Readable name of the content item source for the publisher to identify it
+     * @example "The Carey Nieuwhof Leadership Podcast"
+     * @example "@AustinChristianUniversity on YouTube"
+     */
+    name: varchar('name', { length: 256 }).notNull(),
+
+    /**
+     * URL to the actual content source
+     * @example "https://christianed.com/courses/biblical-theology"
+     */
+    url: varchar('url', { length: 500 }).notNull(),
+
+    /**
+     * Is content supposed to be auto-synced
+     * @example true
+     * @example false
+     */
+    autoSync: boolean('auto_sync').default(false).notNull(),
+    // TODO: consider using sync_frequency instead, with null value for manual sync only
+
+    /**
+     * Type-specific data for the content item source
+     * @example {}
+     */
+    data: jsonb('data').notNull(),
+
+    /**
+     * Sync-related timestamps to determine its current status in the UI and get the idea on how long it takes to sync
+     */
+    lastSyncStartedAt: timestamp('last_sync_started_at').defaultNow().notNull(),
+    lastSyncFinishedAt: timestamp('last_sync_finished_at')
+      .defaultNow()
+      .notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  }
+);
+
 export const contentItems = contentProxySchema.table('content_items', {
   /**
    * Unique identifier for the content item (UUID v4)
@@ -36,6 +106,13 @@ export const contentItems = contentProxySchema.table('content_items', {
   publisherId: varchar('publisher_id', { length: 12 })
     .notNull()
     .references(() => publishers.id),
+
+  /**
+   * Optional foreign key reference to the content item source
+   */
+  contentItemsSourcesId: varchar('content_items_source_id', {
+    length: 12,
+  }).references(() => contentItemsSources.id),
 
   /**
    * Type of content
