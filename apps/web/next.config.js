c2cb245123db0cb061e5c1edb6f4bdb7fc08207a
Caleb Sattgast <calebsattgast@gmail.com>
2025-10-08 21:58:23 -0600

    update content proxy to allow wildcard origins (better support for vercel deployments (#19)
    * also added more next <Image> configs to support more test image sources

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@repo/ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
